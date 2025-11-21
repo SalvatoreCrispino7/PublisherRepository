8aafd6c0ee797033003d12b34280f2294e394079
Salvatore <Salvatore.crispino@fermi.mo.it>
2025-11-21 21:14:52 +0100

    setup per popolare inizialmente il database

--- setup_database.py
+++ setup_database.py
@@ -1,4 +1,7 @@
+import pymongo
+import asyncio
 
+client = pymongo.AsyncMongoClient("mongodb://localhost:27017")
 db = client["publisher_db"]
 publishers_collection = db["publishers"]
 books_collection = db["books"]
@@ -9,18 +12,12 @@ async def main():
         "founded_year": 1907,
         "country": 'Italia'
     })
-
     pub_id = str(pub.inserted_id)
-
     await books_collection.insert_one({
         "publisher_id": pub_id,
         "name": 'Libro Mondadori',
         "year": 2024
     })
 
-await main()
-
-cursor = publishers_collection.find({})
-async for documento in cursor:
-    documento["_id"] = str(documento["_id"])
-    data["lista"].append(documento)
+if __name__ == '__main__':
+    asyncio.run(main())
