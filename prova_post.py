91f12c808f61d2eda82ecbdd7dc10f0e4aa1a915
Salvatore <Salvatore.crispino@fermi.mo.it>
2025-11-21 22:11:46 +0100

    aggiunta put publisher/publisher_id

--- prova_post.py
+++ prova_post.py
@@ -1,11 +1,14 @@
 import subprocess
 
+# Sostituisci <id> con il valore reale dell'ID da aggiornare
+id_editore = "6920cf6f97d2026da822c4b6"
+
 curl_command = [
     "curl",
-    "-X", "POST",
-    "http://localhost:8888/publishers",
+    "-X", "PUT",
+    f"http://localhost:8888/publishers/{id_editore}",
     "-H", "Content-Type: application/json",
-    "-d", '{"name": "prova", "founded_year": 1907, "country": "Italia"}'
+    "-d", '{"name": "prova aggiornata", "founded_year": 1910, "country": "Italia"}'
 ]
 
 result = subprocess.run(curl_command, capture_output=True, text=True)
